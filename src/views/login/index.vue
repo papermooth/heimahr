db4e52acc921c3b78710e8cf146f259ba3bf4558
bufferhhdeMacBook-Pro <bufferhh@bufferhhdeMacBook-Pro>
2024-04-21 16:57:25 +0800

    表单校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,20 +5,20 @@
       <h1>登录</h1>
       <el-card shadow="never" class="login-card">
         <!--登录表单-->
-        <el-form>
-          <el-form-item>
-            <el-input placeholder="请输入手机号" />
+        <el-form ref="form" :model="loginForm" :rules="loginRules">
+          <el-form-item prop="mobile">
+            <el-input v-model="loginForm.mobile" placeholder="请输入手机号" />
           </el-form-item>
-          <el-form-item>
-            <el-input placeholder="请输入密码" />
+          <el-form-item prop="password">
+            <el-input v-model="loginForm.password" show-password placeholder="请输入密码" />
           </el-form-item>
-          <el-form-item>
-            <el-checkbox>
+          <el-form-item prop="isAgree">
+            <el-checkbox v-model="loginForm.isAgree">
               用户平台使用协议
             </el-checkbox>
           </el-form-item>
           <el-form-item>
-            <el-button style="width:350px" type="primary">登录</el-button>
+            <el-button style="width:350px" type="primary" @click="login">登录</el-button>
           </el-form-item>
         </el-form>
       </el-card>
@@ -27,7 +27,58 @@
 </template>
 <script>
 export default {
-  name : "Login"
+  name : "Login",
+  data() {
+    return {
+      loginForm: {
+        mobile: '',
+        password: '',
+        isAgree: false
+      },
+      loginRules: {
+        mobile: [{
+          required: true,
+          message: '请输入手机号',
+          trigger: 'blur'
+        }, {
+          pattern: /^1[3-9]\d{9}$/,
+          message: '手机号格式不正确',
+          trigger: 'blur'
+
+        }],
+        password: [{
+          required: true,
+          message: '请输入密码',
+          trigger: 'blur'
+        }, {
+          min: 6,
+          max: 16,
+          message: '密码长度应该为6-16位之间',
+          trigger: 'blur'
+
+        }],
+        // required只能检查 null "" undefined
+        isAgree: [{
+          validator: (rule, value, callback) => {
+            // rule规则
+            // value检查的数据 true/false
+            // callback 函数 执行这个函数
+            // 成功执行callback 失败也执行callback(错误对象 new Error(错误信息))
+            value ? callback() : callback(new Error('没有勾选用户平台协议'))
+          }
+        }]
+      }
+    }
+  },
+  methods: {
+    login() {
+      this.$refs.form.validate((isOK) => {
+        if (isOK) {
+          alert('校验通过')
+        }
+      })
+    }
+  }
 }
 </script>
 <style lang="scss">
