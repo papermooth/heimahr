81fba8aa0c9adb02443faae5bb0f5cb3264e5ba5
bufferhhdeMacBook-Pro <bufferhh@bufferhhdeMacBook-Pro>
2024-04-22 09:18:48 +0800

    组织架构-节点数据的展示

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="container">
     <div class="app-container">
-        <el-tree default-expand-all :data="depts" :props="defaultProps" />
+        <el-tree default-expand-all :data="depts" :props="defaultProps">
+        <!-- 节点结构 -->
+        <!-- v-slot="{ node, data }" 只能作用在template -->
+        <template v-slot="{ data }">
+          <el-row style="width:100%;height:40px" type="flex" justify="space-between" align="middle">
+            <el-col>{{ data.name }}</el-col>
+            <el-col :span="4">
+              <span class="tree-manager">{{ data.managerName }}</span>
+              <!-- $event 实参 表示类型 -->
+              <el-dropdown >
+                <!-- 显示区域内容 -->
+                <span class="el-dropdown-link">
+                  操作<i class="el-icon-arrow-down el-icon--right" />
+                </span>
+                <!-- 下拉菜单选项 -->
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+                  <el-dropdown-item command="edit">编辑部门</el-dropdown-item>
+                  <el-dropdown-item command="del">删除</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </el-col>
+          </el-row>
+        </template>
+  </el-tree>
     </div>
   </div>
 </template>
@@ -11,11 +35,11 @@ export default {
   data() {
     return  {
       depts: [{
-        name: '传智教育',
+        name: '传智教育',managerName:'管理员',
         children: [
-          { name: '总裁办'},
-          { name: '行政部'},
-          { name: '财务部'}
+          { name: '总裁办',managerName:'张三'},
+          { name: '行政部',managerName:'李四'},
+          { name: '财务部',managerName:'王五'}
         ]
       }],
       defaultProps: {
@@ -30,5 +54,10 @@ export default {
 .app-container {
     padding: 30px 140px;
     font-size:14px;
+}
+.tree-manager {
+  width: 50px;
+  display: inline-block;
+  margin: 10px;
 }
 </style>
